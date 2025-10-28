7cc0d331dc028541b21f0edb2704802f21b1f0cf
Vĩnh Cơ <23520189@gm.uit.edu.vn>
2025-10-28 10:26:33 +0000

    feat: Add Dockerfiles and configuration for microservices, update dependencies, and enhance .dockerignore

--- apps/trip-service/src/app.module.ts
+++ apps/trip-service/src/app.module.ts
@@ -1,31 +1,34 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TripModule } from './trip/trip.module';
-import path from 'path';
-import { RabbitmqModule } from '@repo/shared';
+  import { Module } from '@nestjs/common';
+  import { ConfigModule, ConfigService } from '@nestjs/config';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { TripModule } from './trip/trip.module';
+  import path from 'path';
+  import { RabbitmqModule } from '@repo/shared';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        url: configService.get<string>('DATABASE_URL'),
-        entities: [path.resolve(__dirname, '.') + '/**/*.entity{.js,.ts}'],
-        synchronize: true,
+  @Module({
+    imports: [
+      ConfigModule.forRoot({ isGlobal: true, expandVariables: true }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          url: configService.get<string>('TRIP_DATABASE_URL'),
+          entities: [path.resolve(__dirname, '.') + '/**/*.entity{.js,.ts}'],
+          synchronize: true,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        }),
       }),
-    }),
-    RabbitmqModule.register({
-      urls: ['amqp://guest:guest@localhost:5672'],
-      exchanges: [
-        { name: 'trip.events', type: 'topic' },
-        { name: 'driver.events', type: 'topic' },
-      ],
-    }),
-    TripModule,
-  ],
-})
-export class AppModule {}
+      RabbitmqModule.register({
+        urls: ['amqp://guest:guest@localhost:5672'],
+        exchanges: [
+          { name: 'trip.events', type: 'topic' },
+          { name: 'driver.events', type: 'topic' },
+        ],
+      }),
+      TripModule,
+    ],
+  })
+  export class AppModule {}
