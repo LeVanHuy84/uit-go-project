8db97d1fecf8d9625264aeb254609ed88103f526
Cơ Quách <23520189@gm.uit.edu.vn>
2025-10-31 11:57:34 +0700

    Dockerconfig (#16)
    * feat: Add Dockerfiles and configuration for microservices, update dependencies, and enhance .dockerignore
    
    * feat: Update environment variables in .env.example, remove trip-service from docker-compose, and configure user-service with new settings

--- apps/trip-service/src/app.module.ts
+++ apps/trip-service/src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TripModule } from './trip/trip.module';
-import path from 'path';
-import { RabbitmqModule } from '@repo/shared';
+  import { Module } from '@nestjs/common';
+  import { ConfigModule, ConfigService } from '@nestjs/config';
+  import { TypeOrmModule } from '@nestjs/typeorm';
+  import { TripModule } from './trip/trip.module';
+  import path from 'path';
+  import { RabbitmqModule } from '@repo/shared';
 
 @Module({
   imports: [
